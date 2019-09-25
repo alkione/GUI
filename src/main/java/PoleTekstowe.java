b8c55ffc7d826b3fb1f24eb32a47e7565e114d5f
mpaszkowska <malgorzatapaszkowska@yahoo.com>
2019-09-25 12:25:40 +0200

    dodano pole tekstowe

---
+++ src/main/java/PoleTekstowe.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class PoleTekstowe implements ActionListener {
+    JTextArea tekst;
+
+    public static void main (String[] args) {
+        PoleTekstowe gui = new PoleTekstowe();
+        gui.tworzGUI();
+    }
+
+    public void tworzGUI() {
+        JFrame ramka = new JFrame();
+        JPanel panel = new JPanel();
+        ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton przycisk = new JButton("Po prostu kliknij");
+        przycisk.addActionListener(this);
+        tekst = new JTextArea(10,20);
+        tekst.setLineWrap(true);
+
+        JScrollPane przewijanie = new JScrollPane(tekst);
+        przewijanie.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        przewijanie.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        panel.add(przewijanie);
+
+        ramka.getContentPane().add(BorderLayout.CENTER, panel);
+        ramka.getContentPane().add(BorderLayout.SOUTH, przycisk);
+
+        ramka.setSize(350, 300);
+        ramka.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent zdarzenie) {
+        tekst.append("przycisk kliknięty \n");
+    }
+}
